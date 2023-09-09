27faf99a847a9515ed2fdb10cea86fa4f7d3025f
HAMZANAIR <hamzanairnair50@gmail.com>
2023-09-09 09:15:51 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function prints the message "Programming is like building
+ * a multilingual puzzle" followed by a new line and returns 0.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
