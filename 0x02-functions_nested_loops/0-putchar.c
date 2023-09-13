594732bbd53a51e8ba407e3be62ad10d27d66bd1
HAMZANAIR <hamzanairnair50@gmail.com>
2023-09-13 05:09:54 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Description: This is the main function that serves as the entry point
+ * for the program. It prints the string "_putchar" to the standard output.
+ *
+ * Return: Always 1 (Exit status)
+ */
+int main(void)
+{
+printf("_putchar\n");
+
+return (1);
+}
