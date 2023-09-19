12adbdc890362ed04198fdce5c0af5dfdc4290eb
HAMZANAIR <hamzanairnair50@gmail.com>
2023-09-19 15:45:41 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* rev_string - Reverses a string.
+* @s: Pointer to the string to be reversed.
+*/
+void rev_string(char *s)
+{
+int length = 0;
+char *start = s;
+char temp;
+
+while (*s != '\0')
+{
+length++;
+s++;
+}
+
+s--;
+
+while (start < s)
+{
+temp = *start;
+*start = *s;
+*s = temp;
+start++;
+s--;
+}
+}
